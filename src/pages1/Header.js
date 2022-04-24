7b037af464229f1c87823ca184f77f90e14c3e34
farzaneh <farzaneh.valaee79@gmail.com>
2022-04-24 04:46:53 -0700

    add dark Mode

--- src/pages1/Header.js
+++ src/pages1/Header.js
@@ -1,15 +1,20 @@
 import React,{useState,useEffect} from 'react';
 import { Outlet,NavLink } from 'react-router-dom';
 import '../Styles/header.scss';
+import '../Styles/darkMode.scss';
+
 
 export default function Header() {
+  const[darkMode,setDarkMode] =useState(false);
   return (
     <>
-    <div className="TopHeader">
+    <div className={darkMode? 'TopHeader darkMode':'TopHeader lightMode'}>
      <h3>Where in the world?</h3>
-     <p>Dark Mode</p>
+     <p onClick={()=>setDarkMode(!(darkMode))}>{darkMode? "Dark Mode" : "Light Mode"}</p>
     </div>
+    <div className={darkMode? 'darkMode':'lightMode'}>
     <Outlet />
+    </div>
     </>
   )
 }
